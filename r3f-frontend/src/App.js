f8d030088a13313cfd6dedf20f9fb04e0443e6df
danieljkpraveen <danieljkpraveen@gmail.com>
2022-08-11 00:19:33 +0530

    completed task 1

--- r3f-frontend/src/App.js
+++ r3f-frontend/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import {Canvas} from "@react-three/fiber"
+import { Model } from './Shuttle'
+import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
+import { KeyLight, FillLight, RimLight } from "./Lighting"
+import { BackDrop, GroundPlane } from "./Stage"
+
+function LoadModel(){
+  return(
+    <mesh>
+      <PerspectiveCamera position={[0,0,1]} fov={75} makeDefault/>
+      <GroundPlane />
+      <BackDrop />
+      <KeyLight brightness={10} color="#d2b4de" /> #purple shade
+      <FillLight brightness={2.6} color="#aed6f1" /> #blue shade
+      <RimLight brightness={54} color="#a2d9ce" /> #green shade
+      <Model />
+      <OrbitControls />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <mesh>
-        <boxGeometry />
-      </mesh>
+    <Canvas style={{ background: "silver" }}>
+      <LoadModel />
     </Canvas>
   );
 }
---
+++ r3f-frontend/src/Stage.js
@@ -0,0 +1,17 @@
+export function GroundPlane() {
+    return (
+        <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>
+            <planeBufferGeometry attach="geometry" args={[500, 500]} />
+            <meshStandardMaterial attach="material" color="white" />
+        </mesh>
+    );
+}
+
+export function BackDrop() {
+    return (
+        <mesh receiveShadow position={[0, -1, -5]}>
+            <planeBufferGeometry attach="geometry" args={[500, 500]} />
+            <meshStandardMaterial attach="material" color="white" />
+        </mesh>
+    );
+}

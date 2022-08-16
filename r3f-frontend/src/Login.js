8fc5d6bc86df32547473f985e2c12dc694c2672d
danieljkpraveen <danieljkpraveen@gmail.com>
2022-08-16 18:34:13 +0530

    minor fix

--- r3f-frontend/src/Login.js
+++ r3f-frontend/src/Login.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 var prev_name = "";
+let responseData;
 
 export function onLogin() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username !== window.prev_name) {
+    if (username !== window.prev_name || username === window.prev_name && responseData === '') {
         window.prev_name = username;
         const options = {
             url: "http://127.0.0.1:8000/api/credentials",
@@ -24,21 +25,30 @@ export function onLogin() {
         if (username && password) {
             axios(options).then((response) => {
                 if (response.data["login"]) {
-                    console.log("Login successfull", username);
+                    responseData = response.data['login'];
+                    console.log(responseData);
                     var btn_id = document.getElementById("login");
                     btn_id.value = username;
                     window.alert("you are now logged in as " + username);
                     <header />;
                 } else {
+                    responseData = response.data['login'];
                     window.btn_name = "Log in";
                     window.alert("Invalid Credentials");
+                    responseData = '';
                 }
             });
         }
     } else if (window.prev_name === username) {
-        window.alert("you will be logged out");
-        var btn_id = document.getElementById("login");
-        btn_id.value = "Log in";
-        window.prev_name = "";
+        console.log(responseData);
+        if(responseData){
+            window.alert("you will be logged out");
+            var btn_id = document.getElementById("login");
+            btn_id.value = "Log in";
+            window.prev_name = "";
+        }else{
+            window.alert("Enter valid credentials");
+            responseData = '';
+        }
     }
 }
